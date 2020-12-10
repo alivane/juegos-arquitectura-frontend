eb0a7e7939f5a9aaa44865fd42159848e25a470f
Jefersson <jeferssonmunoz6@gmail.com>
2020-12-10 18:21:12 -0500

    Solucionando error en title home

--- src/containers/Home/title.jsx
+++ src/containers/Home/title.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 
-
-
-export default function Title(){
-
-return(
-
-
-  <div className="header">
-    <h className="head">B</h>
-    <h className="head2">U</h>
-    <h className="head3">I</h>
-    <h className="head4">L</h>
-    <h className="head5">D</h>
-    <h className="head6">S</h>
-      <h className="head7">I</h>
-      <h className="head8">G</h>
-      <h className="head9">N</h>
-   
-  </div>
-
+export default function Title() {
+  return (
+    <div className="header">
+      <h1 className="head">B</h1>
+      <h1 className="head2">U</h1>
+      <h1 className="head3">I</h1>
+      <h1 className="head4">L</h1>
+      <h1 className="head5">D</h1>
+      <h1 className="head6">S</h1>
+      <h1 className="head7">I</h1>
+      <h1 className="head8">G</h1>
+      <h1 className="head9">N</h1>
+    </div>
   );
-
 }
